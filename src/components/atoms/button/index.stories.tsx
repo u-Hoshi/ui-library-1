3f6e45403d3ae91ec5320201f231fc9601858dd2
u-Hoshi <yuta000707@yahoo.co.jp>
2023-07-07 23:37:57 +0900

    フォルダ名を変更してstoriesを追加(ローカルでは/Buttonになっているのにリモートだと/buttonになっている)

---
+++ src/components/atoms/button/index.stories.tsx
@@ -0,0 +1,22 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import CustomButton from './index';
+
+const meta: Meta<typeof CustomButton> = {
+  component: CustomButton,
+  args: {
+    text: 'sample',
+    onClick: () => alert('ボタンがクリックされました'),
+    color: 'primary',
+  },
+};
+
+export default meta;
+
+export const Primary: StoryObj<typeof CustomButton> = {
+  args: {
+    text: 'hoge',
+    onClick: () => alert('ボタンがクリックされました'),
+    color: 'primary',
+  },
+};
