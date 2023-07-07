b890834d356b7bce2a032b4d38e093514fa54041
u-Hoshi <yuta000707@yahoo.co.jp>
2023-07-07 23:39:44 +0900

    パスの変更

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,4 +1,4 @@
-export { default as Button } from './Button/index';
+export { default as Button } from './button/index';
 export { default as Avatar } from './Avatar/index';
 export { default as Box } from './Box/index';
 export { default as IconButton } from './IconButton/index';
