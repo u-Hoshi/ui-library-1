48e5d9f3ce17995e9cf48aa45e0f4f7089e0ec2a
u-Hoshi <yuta000707@yahoo.co.jp>
2023-07-08 10:11:58 +0900

    stories修正

--- src/components/atoms/Box/index.stories.tsx
+++ src/components/atoms/Box/index.stories.tsx
@@ -6,7 +6,7 @@ const meta: Meta<typeof CustomBox> = {
   component: CustomBox,
   args: {
     sx: {
-      bgcolor: 'green',
+      bgcolor: 'primary.main',
     },
     children: 'hoge',
   },
@@ -17,7 +17,7 @@ export default meta;
 export const Primary: StoryObj<typeof CustomBox> = {
   args: {
     sx: {
-      bgcolor: 'red',
+      bgcolor: 'primary.main',
     },
     children: 'hoge',
   },
