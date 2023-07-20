a657c3ec538c603a8b35f6909399709739fa3949
u-Hoshi <yuta000707@yahoo.co.jp>
2023-07-20 22:09:16 +0900

    Typographyのストーリー実装

--- src/components/atoms/Typography/index.stories.tsx
+++ src/components/atoms/Typography/index.stories.tsx
@@ -15,6 +15,29 @@ const meta: Meta<typeof Typography> = {
     color: {
       control: 'color',
     },
+    align: {
+      control: 'radio',
+      options: ['left', 'center', 'right', 'justify'],
+    },
+    variant: {
+      control: 'radio',
+      options: [
+        'body1',
+        'body2',
+        'button',
+        'caption',
+        'h1',
+        'h2',
+        'h3',
+        'h4',
+        'h5',
+        'h6',
+        'inherit',
+        'overline',
+        'subtitle1',
+        'subtitle2',
+      ],
+    },
   },
 };
 
